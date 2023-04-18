6955d0429d7fc0083f912af560dc0a71aaacce8f
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-18 18:23:14 +0900

    feat: Img 컴포넌트 props 추가#3
    - className props 추가

--- src/components/Img/index.tsx
+++ src/components/Img/index.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Image from "next/image";
+import styled from "@emotion/styled";
 
 interface ImgProps {
   src: string;
   size: { w: number; h: number };
   alt: string;
+  className?: string;
 }
 
-function Img({ src, size, alt }: ImgProps) {
-  return <Image src={src} width={size.w} height={size.h} alt={alt} priority />;
+function Img({ src, size, alt, className }: ImgProps) {
+  return (
+    <Container
+      src={src}
+      size={size}
+      width={size.w}
+      height={size.h}
+      alt={alt}
+      className={className}
+      priority
+    />
+  );
 }
 
 export default Img;
+
+const Container = styled(Image)<Pick<ImgProps, "size">>`
+  width: ${props => props.size.w}px;
+  height: ${props => props.size.h}px;
+`;
