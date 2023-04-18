3e84cd6517e3056b2fdc945bbe6f86b36ccaa3bf
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-18 17:46:53 +0900

    refactor: 이더리움 이미지 변경 적용 #2
    - 이더리움 이미지 변경 후 assets/imgs/index.tsx 적용

--- src/assets/imgs/index.tsx
+++ src/assets/imgs/index.tsx
@@ -1,4 +1,4 @@
-const ethereumLogo = "/ethereum-logo.webp";
+const ethereumLogo = "/ethereum.webp";
 const renaissanceLabLogo = "/renaissance-lab-logo.webp";
 const arrowButton = "/arrow-button.webp";
 
