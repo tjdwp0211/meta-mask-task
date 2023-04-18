134327d1562a931d302a180c8f4aa4bd9b2f9285
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-18 16:35:42 +0900

    style: img src Export #3
    - public에 존재하는 img src 경로 src/assets/imgs에서 export

---
+++ src/assets/imgs/index.tsx
@@ -0,0 +1,5 @@
+const ethereumLogo = "/ethereum-logo.webp";
+const renaissanceLabLogo = "/renaissance-lab-logo.webp";
+const arrowButton = "/arrow-button.webp";
+
+export { ethereumLogo, renaissanceLabLogo, arrowButton };
