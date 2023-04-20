0834b6715f2b7541217b560802f9c34b297b8dcd
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-20 14:31:37 +0900

    feat: index page 관련 #5
    - index page layout 생성
    - API getServerSideProps 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,65 @@
 import React from "react";
-import Image from "next/image";
 import styled from "@emotion/styled";
+import { Poster } from "../components";
+import { GetServerSidePropsContext } from "next";
+import IMG_API from "../api/Image";
 
-function index() {
-  return (
-    <Wrapper>
-      HELLO WORLD
-      <Image
-        src={"/renaissance-lab.svg"}
-        width={100}
-        height={24}
-        alt="logo"
-      ></Image>
-    </Wrapper>
-  );
+function index({ data }) {
+  if (!data) {
+    return <>loading..</>;
+  } else {
+    const slicedDatas = data.reduce((acc, cur, i) => {
+      if (i % 5 === 0) acc.push([cur]);
+      else acc[acc.length - 1].push(cur);
+
+      return acc;
+    }, []);
+
+    return (
+      <Container>
+        {slicedDatas.map((data, i: number) => (
+          <Poster key={i} data={data} />
+        ))}
+      </Container>
+    );
+  }
 }
 
 export default index;
 
-const Wrapper = styled.div``;
+const Container = styled.div`
+  width: clac(100% - 48px);
+  height: fit-content;
+  padding-top: 70px;
+  display: grid;
+  justify-content: center;
+  justify-items: center;
+  grid-template-columns: repeat(3, clamp(300px, 30%, 381px));
+  grid-template-rows: repeat(auto-fill, 331px);
+  gap: 24px;
+  @media (max-width: 991px) {
+    width: 100%;
+    grid-template-columns: repeat(2, minmax(calc(50% - 24px), 381px));
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    grid-template-columns: repeat(1, 381px);
+  }
+`;
+
+interface GetImg {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  console.log(context);
+  const data = await IMG_API.get().then(res =>
+    res.data.map((el: GetImg) => ({ src: el.url }))
+  );
+
+  return { props: { data } };
+}
