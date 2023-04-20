62d587d2df4214784e34d41de658498847f2f2e8
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-20 14:29:29 +0900

    build: axios package 추가 #5
    - axios package 추가
    - next.config.js에 images key 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: { emotion: true },
+  images: {
+    domains: ["via.placeholder.com"],
+  },
 };
 
 module.exports = nextConfig;
