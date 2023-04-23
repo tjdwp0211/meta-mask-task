5b8d391e4f0dba8c3f15aca09fad2d7c47ed057f
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-23 22:37:51 +0900

    refactor: Popup export #6
    - components/index.ts 에서 Popup export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Img from "./Img";
 import Text from "./Text";
 import Navbar from "./Navbar";
 import Poster from "./Poster";
+import Popup from "./Popup";
 
-export { Img, Text, Navbar, Poster };
+export { Img, Text, Navbar, Poster, Popup };
