4d219155492107bd12ed23ac3a4bed60f38edcbe
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-23 23:51:32 +0900

    refactor: components types 생성 #7
    - types/components/index.ts 생성 후 타입들 긁어 모음

---
+++ src/types/components/index.ts
@@ -0,0 +1,35 @@
+import React, { Dispatch } from "react";
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface PopupProps {
+  view: boolean;
+  setPipeLineWithPopup: Dispatch<
+    React.SetStateAction<{
+      view: boolean;
+      account: null | string;
+      balance: null | string;
+    }>
+  >;
+  deleteJazziconRef(): void;
+}
+
+export interface PosterProps {
+  data: { title?: string; src: string }[];
+}
+
+export interface TextProps {
+  family?: string;
+  size?: number;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export interface ImgProps {
+  src: string;
+  size: { w: number; h: number };
+  alt: string;
+  className?: string;
+}
