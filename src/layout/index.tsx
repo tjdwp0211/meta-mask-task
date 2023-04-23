50ff5c3bd8983a72e2dfe011507c0c288e21c506
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-23 22:39:49 +0900

    feat: @metamask/jazzicon #6
    - user login 시 profile 사진 표출 @metamask/jazzicon으로 사용

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
-import { Img, Navbar, Text } from "../components";
-import { arrowButton, renaissanceLabLogo } from "../assets/imgs";
-import { PALETTE, URBANIST } from "../styles";
+import { Img, Navbar, Popup, Text } from "../components";
+import { arrowButton, ethereumLogo, renaissanceLabLogo } from "../assets/imgs";
+import { AIRBNBCEREAL_W, PALETTE, URBANIST } from "../styles";
+import Jazzicon from "@metamask/jazzicon";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
+  const jazziconRef = useRef<HTMLDivElement>(null);
+  const [pipeLineWithPopup, setPipeLineWithPopup] = useState({
+    view: false,
+    account: null,
+    balance: null,
+  });
+
   const handleArrowButtonClick = () => {
-    console.log("Arrow Button Click");
+    setPipeLineWithPopup(prev => ({ ...prev, view: !prev.view }));
+  };
+
+  const handleJazziconRef = () => {
+    jazziconRef.current.firstChild.remove();
+  };
+
+  useEffect(() => {
+    if (pipeLineWithPopup.account && jazziconRef.current) {
+      const userIcon: HTMLDivElement = Jazzicon(16, pipeLineWithPopup.account);
+      userIcon.style.width = "40px";
+      userIcon.style.height = "40px";
+
+      jazziconRef.current.appendChild(userIcon);
+    }
+  }, [pipeLineWithPopup.account]);
+
+  const createEthereumBalance = () => {
+    return (
+      <EthereumBalanceWrapper>
+        <Img
+          src={ethereumLogo}
+          size={{ w: 25, h: 25 }}
+          alt="Ethereum balance"
+        />
+        <Text family={AIRBNBCEREAL_W.bold} size={18}>
+          {pipeLineWithPopup.balance} ETH
+        </Text>
+      </EthereumBalanceWrapper>
+    );
   };
 
   return (
@@ -21,10 +58,16 @@ function Layout({ children }: LayoutProps) {
           Renaissance Lab.
         </MainLogoWrapper>
         <UserProfileWrapper>
-          <div></div>
+          {pipeLineWithPopup.account && createEthereumBalance()}
+          <div className="user-profile" ref={jazziconRef} />
           <button onClick={handleArrowButtonClick}>
             <Img src={arrowButton} size={{ w: 24, h: 24 }} alt="opener" />
           </button>
+          <Popup
+            view={pipeLineWithPopup.view}
+            setPipeLineWithPopup={setPipeLineWithPopup}
+            deleteJazziconRef={handleJazziconRef}
+          />
         </UserProfileWrapper>
       </Header>
       <Navbar />
@@ -66,10 +109,29 @@ const UserProfileWrapper = styled.aside`
   display: flex;
   align-items: center;
   gap: 4px;
-  & > div {
+  height: 100%;
+  position: relative;
+  .user-profile {
     width: 40px;
     height: 40px;
     border-radius: 50%;
+    margin-left: 20px;
     background-color: ${PALETTE.gray.dark};
+  }
+`;
+
+const EthereumBalanceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  p:nth-of-type(1) {
+    color: ${PALETTE.black.soft};
+  }
+  img {
+    width: 16px;
+    height: 16px;
+    padding: 4px;
+    border-radius: 50%;
+    background-color: ${PALETTE.blue.light};
   }
 `;
