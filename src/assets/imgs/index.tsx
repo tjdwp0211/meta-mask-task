f4734f12326a08c818cd0634b9654733da90d595
Sung Je Kim <rlatjdwp0211@naver.com>
2023-04-23 22:35:52 +0900

    build: metamask logo 추가 #1
    - metamask-logo.png 추가

--- src/assets/imgs/index.tsx
+++ src/assets/imgs/index.tsx
@@ -1,5 +1,6 @@
 const ethereumLogo = "/ethereum.webp";
 const renaissanceLabLogo = "/renaissance-lab-logo.webp";
+const metamaskLogo = "/metamask-logo.png";
 const arrowButton = "/arrow-button.webp";
 
-export { ethereumLogo, renaissanceLabLogo, arrowButton };
+export { ethereumLogo, renaissanceLabLogo, metamaskLogo, arrowButton };
